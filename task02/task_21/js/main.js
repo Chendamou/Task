f34181cf4c1cdd876d2811567fa91e8a53d0ffd6
imflynn <flynn0101@163.com>
2016-03-30 23:41:31 +0800

    task02

--- task02/task_21/js/main.js
+++ task02/task_21/js/main.js
@@ -1,15 +1,3 @@
-/**
- * Created by flynn on 2016/3/29.
- */
-
-if (String.prototype.trim === undefined) {
-    String.prototype.trim = function () {
-        return this.replace(/^\s+/, '').replace(/\s+$/, '');
-    }
-}
-
-
-
 var Utils = {
     addHandler: function (ele, type, handler) {
         if (ele.addEventListener) {
@@ -21,150 +9,118 @@ var Utils = {
         }
     },
 
+    each : function (arr,fn) {
+        for (var i = 0; i < arr.length; i++) {
+            fn(arr[i], i);
+        }
+    },
+
     splitInput : function (text) {
         var inputArr = [];
-        var splitRE = /[\s\r\n、,，]+/;
-        inputArr = text.split(splitRE);
+        inputArr = (text).split(/[,，;；、\s\n]+/);
         return inputArr;
     },
 
-    isEmpty : function (val) {
-        if (val.length == 0) {
-            return true;
-        }
-    },
-
-    render : function (arr) {
-        var main = document.getElementById('main');
-        for (var i = 0; i  < arr.length; i++) {
-            var span = document.createElement('span');
-            span.className = 'sp';
-            span.id = i;
-            span.innerHTML = arr[i];
-            main.appendChild(span);
-        }
-    },
-    emptyMain : function () {
-        var main = document.getElementById('main');
-        main.innerHTML = '';
-    },
-
-    getInputArray : function () {
-        var ta = document.getElementById('ta');
-        var ta1 = ta.value;
-        if (ta1 == '') {
-            alert("不能为空!");
-        }
-        else {
-            var ta2 = ta1.trim();
-            return Utils.splitInput(ta2);
-        }
-    },
-    getSearchText : function () {
-        var search_text = document.getElementById('search_text');
-        var main = document.getElementById('main');
-        var search_value = search_text.value;
-        for (var i = 0; i < main.childNodes.length; i++) {
-            if (main.childNodes[i].innerHTML.indexOf(search_value) != -1) {
-                console.log(main.childNodes[i].innerHTML);
-                main.childNodes[i].style.color = 'green';
-                main.childNodes[i].style.background = '#ccc';
-            }
-        }
-    }
-};
-
-var Queue = {
-    addLeft: function (box) {
-        Utils.emptyMain();
-        var arr = Utils.getInputArray();
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] != '') {
-                box.unshift(arr[i]);
-            }
-        }
-        return box;
-    },
-    addRight: function (box) {
-        Utils.emptyMain();
-        var arr = Utils.getInputArray();
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] != '') {
-                box.push(arr[i]);
-            }
-        }
-        return box;
-    },
-    removeLeft: function (box) {
-        Utils.emptyMain();
-        if (Utils.isEmpty(box)) {
-            alert("已经没东西可删了哦！")
-        }
-        else {
-            var a = box.shift();
-            alert(a);
-        }
-    },
-    removeRight: function (box) {
-        Utils.emptyMain();
-        if (Utils.isEmpty(box)) {
-            alert("已经没东西可删了哦！")
-        }
-        else {
-            var a = box.pop();
-            alert(a);
-        }
-    },
-    removeCenter: function (box) {
-        Utils.emptyMain();
-        if (Utils.isEmpty(box)) {
-            alert("已经没东西可删了哦！")
-        }
-        else {
-            var node = event.target;
-            var aId = node.id;
-            var a = box.splice(aId,1);
-            alert(a);
-        }
+    trim: function (str) {
+        return str.replace(/^\s+/, '').replace(/\s+$/, '');
     }
 
 };
+
+function Queue(container, isDelDiv) {
+    this.str = [];
+
+    this.addLeft = function(obj) {
+        if (obj.length) {
+            this.str.unshift(obj);
+            this.render();
+        }
+    };
+
+    this.addRight = function(obj) {
+        if (obj.length) {
+            this.str.push(obj);
+            this.render();
+        }
+    };
+
+    this.isEmpty = function() {
+        return (this.str.length == 0);
+    };
+
+    this.removeLeft = function() {
+        if (!this.isEmpty()) {
+            this.str.shift();
+            this.render();
+        }
+        else {
+            alert("没东西可删了o");
+        }
+    };
+
+    this.removeRight = function() {
+        if (!this.isEmpty()) {
+            this.str.pop();
+            this.render();
+        }
+        else {
+            alert("没东西可删了o");
+        }
+    };
+
+    this.render = function() {
+        var str = "";
+        Utils.each(this.str, function(item){str += ("<span class='span_1'>" + item + "</span>")});
+        container.innerHTML = str;
+        if(isDelDiv) addDivDelEvent(this, container);
+    };
+
+    this.removeCenter = function(id) {
+        this.str.splice(id, 1);
+        this.render();
+    }
+}
+
+function addDivDelEvent(Queue, container) {
+    var temp = [];
+    for (var i = 0; i < container.childNodes.length; i++) {
+        temp.push(container.childNodes[i].innerHTML);
+        //这里要使用闭包，否则永远绑定到指定div上的delete函数的参数永远等于跳出时的cur值(length);
+        Utils.addHandler(container.childNodes[i], "click", function() {
+            return function(){return Queue.removeCenter(i)};
+        }(i));
+        // Utils.addHandler(container.childNodes[i], "mouseover", function(i) {
+        //     return function(){container.childNodes[i].style.background = "green"; container.childNodes[i].innerHTML = "删除" + temp[i] + "?";};
+        // }());
+        // Utils.addHandler(container.childNodes[i], "mouseout", function(i) {
+        //     return function(){container.childNodes[i].style.background = "red"; container.childNodes[i].innerHTML = temp[i];};
+        // }(i));
+    }
+}
 
 window.onload = function () {
-    var box = [];
-    var left_in = document.getElementById('left_in');
-    var right_in = document.getElementById('right_in');
-    var left_out = document.getElementById('left_out');
-    var right_out = document.getElementById('right_out');
-    var main = document.getElementById('main');
-    var btn = document.getElementById('search_btn');
+    var container_1 = document.getElementById('container_1');
+    var tag_input = document.getElementById('tag_input');
+    var container_2 = document.getElementById('container_2');
+    var text_area = document.getElementById('text_area');
+    var btn = document.getElementById('btn');
 
-    Utils.addHandler(btn, 'click', Utils.getSearchText);
-
-    Utils.addHandler(left_in,'click',function () {
-        Queue.addLeft(box);
-        Utils.render(box);
-    });
-
-
-    Utils.addHandler(right_in,'click',function () {
-        Queue.addRight(box);
-        Utils.render(box);
-    });
-
-    Utils.addHandler(left_out,'click',function () {
-        Queue.removeLeft(box);
-        Utils.render(box);
-    });
-
-    Utils.addHandler(right_out,'click',function () {
-        Queue.removeRight(box);
-        Utils.render(box);
-    });
-
-    Utils.addHandler(main,'click',function () {
-        Queue.removeCenter(box);
-        Utils.render(box);
+    var tagQueue = new Queue(container_1, true);
+    var interestQueue = new Queue(container_2, false);
+    
+    Utils.addHandler(tag_input, 'keyup', function (e) {
+        if (/[,，;；、\s\n]+/.test(tag_input.value) || e.keyCode ===13) {
+            var handledData = Utils.splitInput(Utils.trim(tag_input.value));
+            var data = handledData[0];
+            if (tagQueue.str.indexOf(data) === -1) {
+                tagQueue.addRight(data);
+                if (tagQueue.str.length > 10) {
+                    tagQueue.removeLeft()
+                }
+            }
+            tagQueue.render();
+            tag_input.value = "";
+        }
     })
 
 };
